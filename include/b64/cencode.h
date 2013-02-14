149cca64719551520e831d999607ab2b4883c00c
Joseph Langley <mightyjo@gmail.com>
2013-02-13 23:53:33 -0500

    Added the GitHub URL to the source files.

--- include/b64/cencode.h
+++ include/b64/cencode.h
@@ -2,6 +2,7 @@
 cencode.h - c header for a base64 encoding algorithm
 
 Copyright 2012 Joseph R. Langley
+https://github.com/Mightyjo/libb64-2
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
